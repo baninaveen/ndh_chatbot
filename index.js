a84c8fb9fab34cc24d8af020152315beae09967c
naveen <baninkumar@gmail.com>
2018-09-16 23:42:08 +0530

    Update code

--- index.js
+++ index.js
@@ -1,26 +1,18 @@
 'use strict';
 
-const {
-    WebhookClient
-} = require('dialogflow-fulfillment');
-const {
-    Card,
-    Suggestion
-} = require('dialogflow-fulfillment');
-
 const express = require('express');
 const request = require('request');
-const BodyParser = require('body-parser');
+const bodyParser = require('body-parser');
+
+
+const app = express();
 
 app.use(bodyParser.json());
-const agent = new WebhookClient({
-    request,
-    response
-});
+
 
 var port = process.env.PORT || 8080;
 // create serve and configure it.
-const app = express();
+
 
 app.get('/webhook', function (req, res) {
     res.send('Hello NDH Chatbot');
