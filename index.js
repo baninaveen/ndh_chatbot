f332e7009f15dd74a3225030b493fa6b0fc81bcf
naveen <baninkumar@gmail.com>
2018-09-16 23:07:37 +0530

    initial code udpate

---
+++ index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const {
+    WebhookClient
+} = require('dialogflow-fulfillment');
+const {
+    Card,
+    Suggestion
+} = require('dialogflow-fulfillment');
+
+const express = require('express');
+const request = require('request');
+const BodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+const agent = new WebhookClient({
+    request,
+    response
+});
+
+var port = process.env.PORT || 8080;
+// create serve and configure it.
+const app = express();
+
+app.post('/webhook', function (req, res) {
+    res.send('Hello NDH Chatbot');
+
+});
+
+app.listen(port, function () {
+    console.log("Server is up and running...");
+});
